81a35f05d4c0cf479bfdd2627f141ed7c82d0a4e
Dan Holland <djolland@gmail.com>
2016-11-22 21:41:43 -0600

    Implemented back button for Bar Profile activity.

--- app/src/main/java/blackout/jester/BarProfileActivity.java
+++ app/src/main/java/blackout/jester/BarProfileActivity.java
@@ -2,6 +2,7 @@ package blackout.jester;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -26,22 +27,21 @@ public class BarProfileActivity extends AppCompatActivity {
             fragment.setArguments(arguments);
 
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.bar_profile_container, fragment)
+                    .replace(R.id.bar_profile_container, fragment)
                     .commit();
         }
 
         Toolbar mToolbar =
                 (Toolbar) rootView.findViewById(R.id.profile_toolbar);
-
         setSupportActionBar(mToolbar);
-        //getActionBar().setHomeAsUpIndicator(R.drawable.ic_action_back);
 
         // Get a support ActionBar corresponding to this toolbar
         android.support.v7.app.ActionBar ab = getSupportActionBar();
 
         // Enable the Up button
-        ab.setDefaultDisplayHomeAsUpEnabled(true);// setDisplayHomeAsUpEnabled(true);
-
+        ab.setHomeButtonEnabled(true);
+        ab.setDisplayHomeAsUpEnabled(true);
+        //ab.setHomeAsUpIndicator(R.drawable.ic_action_back);
     }
 
     @Override
@@ -55,8 +55,8 @@ public class BarProfileActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
+            case  android.R.id.home:
+                this.finish();
                 return true;
 
             case R.id.action_favorite:
