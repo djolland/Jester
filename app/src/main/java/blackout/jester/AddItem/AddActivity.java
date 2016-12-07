93aef6e0b3ab596e1d457ea1ee2602d27ef97dd0
isurchmnd <isurchmnd@gmail.com>
2016-12-06 19:37:37 -0600

    removing uneeded files

--- app/src/main/java/blackout/jester/AddItem/AddActivity.java
+++ app/src/main/java/blackout/jester/AddItem/AddActivity.java
@@ -1,7 +1,14 @@
 package blackout.jester.AddItem;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class AddActivity {
-
+public class AddActivity extends AppCompatActivity
+{
 
 }
